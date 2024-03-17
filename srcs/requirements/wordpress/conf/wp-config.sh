3943a83e61b82d5312f7832c52356824745e9bbc
Oussama Lahmami <o.lahmami.2000@gmail.com>
2024-03-17 14:55:12 +0100

    replace with URL var

--- srcs/requirements/wordpress/conf/wp-config.sh
+++ srcs/requirements/wordpress/conf/wp-config.sh
@@ -5,8 +5,8 @@ wp core download --allow-root
 wp config create --dbhost=$DBHOST --dbname=$DBNAME --dbuser=$DBUSER --dbpass=$DBPASS --allow-root
 wp core install --url=$URL --title=$TITLE --admin_user=$ADMIN_USER --admin_password=$ADMIN_PASS --admin_email=$ADMIN_MAIL --allow-root
 wp user create $OTHER_USER $OTHER_MAIL --role=subscriber --user_pass=$OTHER_PASS --allow-root
-wp option update home 'https://olahmami.42.fr'
-wp option update siteurl 'https://olahmami.42.fr'
+wp option update home '$URL'
+wp option update siteurl '$URL'
 
 # start php-fpm
 /usr/sbin/php-fpm8.2 -F
